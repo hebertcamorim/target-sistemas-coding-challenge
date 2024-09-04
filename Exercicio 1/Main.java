7ae63de46be74ca1895bdda9c304cbf2951c0004
Hebert Cesar Amorim <59668749+hebertcamorim@users.noreply.github.com>
2024-09-04 19:51:43 -0300

    Exercicio 1

---
+++ Exercicio 1/Main.java
@@ -0,0 +1,12 @@
+//A escolha da linguagem foi JAVA
+
+public class Main {
+    public static void main(String[] args) {
+      int indice = 13, soma = 0, k = 0;
+      while(k < indice) {
+        k++;
+        soma+= + k;
+      }
+      System.out.print(soma);
+  }
+}
